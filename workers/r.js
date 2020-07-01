a22d998fe4f5ef345399c702519ec65bdcef3ccf
Nick Cao <nickcao@nichi.co>
2020-07-01 10:07:22 +0800

    update to reverse proxy api

--- workers/r.js
+++ workers/r.js
@@ -1,21 +1,24 @@
 addEventListener("fetch", event => {
-  event.respondWith(handler(event));
+    event.respondWith(handler(event.request));
 });
 
-async function handler(event) {
-  let src
-  try {
-    src = new URL((new URL(event.request.url)).searchParams.get('src'))
-  }
-  catch (e) {
-    return new Response("invalid or missing query param: src", { status: 400 })
-  }
-  response = await fetch(src, event.request);
-  if (response.status == 302 || response.status == 301) {
-    return Response.redirect(new URL(response.headers.get("Location"), src), 302);
-  }
-  response = new Response(response.body, response)
-  response.headers.set('Access-Control-Allow-Origin', '*')
-  return response;
+async function handler(request) {
+    let url
+    try {
+        url = new URL((new URL(request.url)).pathname.substr(1).replace(/(?<=^https?:)\/(?!\/)/, '//'))
+    } catch {
+        return new Response('invalid url', { status: 400 })
+    }
+
+    response = await fetch(url, request);
+    if (response.status == 302 || response.status == 301) {
+        return Response.redirect((new URL(request.url)).origin.
+            concat('/').
+            concat(new URL(response.headers.get("Location"), url), 302))
+    }
+    response = new Response(response.body, response)
+    response.headers.set('Access-Control-Allow-Origin', '*')
+    response.headers.set('X-REAL-URL', url)
+    return response;
 }
 
