4b4d379e5d8df8daab638afb46b983869217b04f
Nick Cao <nickcao@nichi.co>
2020-07-20 09:21:56 +0800

    updated r.js

--- workers/r.js
+++ workers/r.js
@@ -8,18 +8,22 @@ String.prototype.fixScheme = function () {
 
 async function handler(request) {
     let origin = (new URL(request.url)).origin
-    let real_url = request.url.substr(origin.length + 1).fixScheme()
+    try {
+        let real_url = request.url.substr(origin.length + 1).fixScheme()
 
-    response = await fetch(real_url, request);
-    if (response.status == 302 || response.status == 301) {
-        let target_url = (new URL(response.headers.get("Location"), real_url)).href
-        return Response.redirect(origin + '/' + target_url, 302)
+        response = await fetch(real_url, request);
+        if (response.status == 302 || response.status == 301) {
+            let target_url = (new URL(response.headers.get("Location"), real_url)).href
+            return Response.redirect(origin + '/' + target_url, 302)
+        }
+
+        response = new Response(response.body, response)
+        response.headers.set('Access-Control-Allow-Origin', '*')
+        response.headers.set('X-REQUEST-URL', request.url)
+        response.headers.set('X-REAL-URL', real_url)
+        return response;
+    } catch (err) {
+        return new Response(`bad request, usage ${origin}/<real url>`, { status: 400 })
     }
-
-    response = new Response(response.body, response)
-    response.headers.set('Access-Control-Allow-Origin', '*')
-    response.headers.set('X-REQUEST-URL', request.url)
-    response.headers.set('X-REAL-URL', real_url)
-    return response;
 }
 
